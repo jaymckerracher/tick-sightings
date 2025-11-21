b17929ed30883f5077fe2b6dc75a5823d1d07118
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-11-21 12:51:56 +0000

    separate map into its own component

---
+++ src/components/Map.jsx
@@ -0,0 +1,18 @@
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+
+export default function Map() {
+    return (
+        <div className="map-wrapper">
+            <MapContainer className="leaflet-map" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[51.505, -0.09]}>
+                    <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </div>
+    );
+}
---
+++ src/pages/Sightings.jsx
@@ -0,0 +1,11 @@
+import Button from '@mui/material/Button';
+import Map from '../components/Map';
+
+export default function Sightings() {
+    return (
+        <>
+            <Button variant="contained">Hello world</Button>
+            <Map />
+        </>
+    );
+}
