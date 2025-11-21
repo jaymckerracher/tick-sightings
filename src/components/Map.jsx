90c1f7b39baa462d62f851db4ae87430daaad08a
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-11-21 13:06:19 +0000

    update map to make it more accessible and easy to read

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,15 +1,28 @@
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 export default function Map() {
+    const ukBounds = [
+        [49.5, -11], // Southwest
+        [61, 2]      // Northeast
+    ];
+
     return (
         <div className="map-wrapper">
-            <MapContainer className="leaflet-map" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+            <MapContainer
+                className="leaflet-map"
+                center={[54.5, -3.5]} // Centered on the UK
+                zoom={6}
+                scrollWheelZoom={true}
+                minZoom={5}
+                maxBounds={ukBounds}
+            >
                 <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+                    attribution='&copy; <a href="https://carto.com/attributions">CARTO</a>'
                 />
-                <Marker position={[51.505, -0.09]}>
+                <Marker position={[54.5, -3.5]}>
                     <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
+                        A pretty CSS3 popup. <br /> Easily customizable.
                     </Popup>
                 </Marker>
             </MapContainer>
