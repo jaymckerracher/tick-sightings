acdbbe156d62ba0fdf77f66898e7bc9a61d02060
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-11-21 12:04:28 +0000

    test material design library

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,14 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+import Map from "./pages/Map";
+
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<></>}/>
+      <Route path="/" element={<Map />}/>
     </Routes>
   </BrowserRouter>,
 );
