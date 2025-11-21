8b3b408b06b61dc8a7f2347c0cd20960d04a3cd7
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-11-21 11:54:11 +0000

    set up react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <></>
-  </StrictMode>,
-)
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<></>}/>
+    </Routes>
+  </BrowserRouter>,
+);
