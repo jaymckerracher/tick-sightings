bf064d3caa0219894abd480c2876b8a1513ed2f8
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-11-21 12:00:24 +0000

    add styling dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const root = document.getElementById("root");
 
