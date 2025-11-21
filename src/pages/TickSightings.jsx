05ff4b2e86e4b46edee73997323774212caa3d10
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-11-21 17:50:27 +0000

    use axios to fetch api data

--- src/pages/TickSightings.jsx
+++ src/pages/TickSightings.jsx
@@ -1,12 +1,21 @@
 // import Button from '@mui/material/Button';
 import { CircularProgress } from '@mui/material';
 import Map from '../components/Map';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function Sightings() {
     const [loading, setLoading] = useState(true)
 
-    
+    useEffect(() => {
+        axios.get('https://dev-task.elancoapps.com/data/tick-sightings')
+            .then(function (response) {
+                console.log('we got data!', response.data)
+            })
+            .catch(function (err) {
+                console.log(err)
+            })
+    }, [])
 
     if (loading) {
         return (
