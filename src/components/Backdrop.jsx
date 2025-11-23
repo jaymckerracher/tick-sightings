e86772aae5fa236e2a6a6669536440de3c958015
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-11-23 02:13:31 +0000

    converting backdrop into reusable component

---
+++ src/components/Backdrop.jsx
@@ -0,0 +1,43 @@
+import { Backdrop, Box, IconButton } from '@mui/material';
+
+export default function Backdrop({ children, state, stateSetter }) {
+    return (
+        <Backdrop
+            open={state}
+            sx={{ color: 'primary.main', zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+            <Box
+                sx={{
+                    bgcolor: 'background.paper',
+                    boxShadow: 24,
+                    borderRadius: 2,
+                    p: 4,
+                    width: '40vw',
+                    height: '80vh',
+                    overflowY: 'auto',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    position: 'relative',
+                }}
+            >
+                <IconButton
+                    size="small"
+                    sx={{
+                        position: 'absolute',
+                        top: 8,
+                        right: 8,
+                        zIndex: 2,
+                    }}
+                    aria-label="close"
+                    onClick={() => {
+                        stateSetter(false)
+                    }}
+                >
+                    <CloseIcon fontSize="small" />
+                </IconButton>
+                
+                {children}
+            </Box>
+        </Backdrop>
+    )
+}
