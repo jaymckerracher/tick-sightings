57217d25d723642d502ba691714ec8d3c65335e3
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-11-23 20:33:07 +0000

    add filter and sort by control UI

--- src/components/TicksPanel.jsx
+++ src/components/TicksPanel.jsx
@@ -1,4 +1,4 @@
-import { Typography, CircularProgress, Link, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+import { Typography, CircularProgress, Link, FormControl, InputLabel, Select, MenuItem, Box } from '@mui/material';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import { useEffect, useState } from 'react';
 import getAllSightings from '../utils/getAllSightings';
@@ -19,6 +19,8 @@ export default function TicksPanel({
     const [ticksPanelLoading, setTicksPanelLoading] = useState(true);
     const [sightingsData, setSightingsData] = useState();
     const [cities, setCities] = useState(["All Cities"]);
+    const [filterSpecies, setFilterSpecies] = useState("All species")
+    const [sortBy, setSortBy] = useState("Date Descending")
 
     let directionsUrl;
     if (ticksPanelCity !== "All cities") {
@@ -28,8 +30,6 @@ export default function TicksPanel({
             directionsUrl = `https://www.google.com/maps/dir/?api=1&origin=My+Location&destination=${ticksPanelCity}`;
         }
     }
-
-    // console.log(getCitiesFromMarkersData())
 
     useEffect(() => {
         setTicksPanelLoading(true);
@@ -63,46 +63,112 @@ export default function TicksPanel({
                 <CircularProgress />
             ) : (
                 <>
-                    <Typography variant="h2" color='secondary.main' sx={{ fontSize: '1.3em', mb: 1 }}>
-                        Sightings in
-                    </Typography>
+                    <Box sx={{ display: "flex", width: "100%", alignItems: "center", justifyContent: "center", p: 1 }}>
+                        <Typography variant="h2" color='secondary.main' sx={{ fontSize: '1.3em', mb: 1 }}>
+                            Sightings in
+                        </Typography>
 
-                    <FormControl fullWidth sx={{ mb: 2 }}>
-                        <InputLabel
-                            id="city-label"
-                            sx={{
-                                color: 'secondary.main',
-                                '&.Mui-focused': {
+                        <FormControl sx={{ width: "35%", m: 2 }}>
+                            <InputLabel
+                                id="city-label"
+                                sx={{
                                     color: 'secondary.main',
-                                },
-                            }}
-                        >
-                            City
-                        </InputLabel>
+                                    '&.Mui-focused': {
+                                        color: 'secondary.main',
+                                    },
+                                }}
+                            >
+                                City
+                            </InputLabel>
 
-                        <Select
-                            labelId="city-label"
-                            id="city-select"
-                            label="City"
-                            value={ticksPanelCity}
-                            onChange={e => setTicksPanelCity(e.target.value)}
-                        >
-                            {cities.map(city => (
-                                <MenuItem value={city}>{city}</MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl>
+                            <Select
+                                labelId="city-label"
+                                id="city-select"
+                                label="City"
+                                value={ticksPanelCity}
+                                onChange={e => setTicksPanelCity(e.target.value)}
+                            >
+                                {cities.map(city => (
+                                    <MenuItem value={city}>{city}</MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
 
-                    {ticksPanelCity === 'All Cities' ? <></> : <Link
-                        href={directionsUrl}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        underline="always"
-                        sx={{ display: 'flex', alignItems: 'center', fontSize: '1.3em', fontWeight: 500, color: 'text.secondary' }}
-                    >
-                        Get Directions
-                        <OpenInNewIcon sx={{ fontSize: '1.1em', ml: 0.5 }} />
-                    </Link>}
+                        {ticksPanelCity === 'All Cities' ? <></> : <Link
+                            href={directionsUrl}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            underline="always"
+                            sx={{ display: 'flex', alignItems: 'center', fontSize: '1.3em', fontWeight: 500, color: 'text.secondary' }}
+                        >
+                            Get Directions
+                            <OpenInNewIcon sx={{ fontSize: '1.1em', ml: 0.5 }} />
+                        </Link>}
+                    </Box>
+
+                    <Box sx={{ width: "100%", mt: 3, display: "flex", justifyContent: "space-evenly", alignItems: "center" }}>
+                        <Box sx={{ width: "40%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+                            <Typography variant='h6' sx={{ color: "secondary.main" }}>Filter species:</Typography>
+
+                            <FormControl fullWidth sx={{ m: 2 }}>
+                                <InputLabel
+                                    id="sighting-filter-species-label"
+                                    sx={{
+                                        color: 'secondary.main',
+                                        '&.Mui-focused': {
+                                            color: 'secondary.main',
+                                        },
+                                    }}
+                                >
+                                    Species
+                                </InputLabel>
+
+                                <Select
+                                    labelId="sighting-filter-species-label"
+                                    id="sighting-filter-species-select"
+                                    label="Species"
+                                    value={filterSpecies}
+                                    onChange={e => setFilterSpecies(e.target.value)}
+                                >
+                                    <MenuItem value="All species" >All species</MenuItem>
+                                    <MenuItem value="Southern rodent tick" >Southern rodent tick</MenuItem>
+                                    <MenuItem value="Tree-hole tick" >Tree-hole tick</MenuItem>
+                                    <MenuItem value="Marsh tick" >Marsh tick</MenuItem>
+                                    <MenuItem value="Passerine tick" >Passerine tick</MenuItem>
+                                    <MenuItem value="Fox/badger tick" >Fox/badger tick</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Box>
+
+                        <Box sx={{ width: "40%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+                            <Typography variant='h6' sx={{ color: "secondary.main" }}>Sort by:</Typography>
+
+                            <FormControl fullWidth sx={{ m: 2 }}>
+                                <InputLabel
+                                    id="sighting-filter-species-label"
+                                    sx={{
+                                        color: 'secondary.main',
+                                        '&.Mui-focused': {
+                                            color: 'secondary.main',
+                                        },
+                                    }}
+                                >
+                                    Species
+                                </InputLabel>
+
+                                <Select
+                                    labelId="sighting-sort-label"
+                                    id="sighting-sort-select"
+                                    label="Sort By"
+                                    value={sortBy}
+                                    onChange={e => setSortBy(e.target.value)}
+                                >
+                                    <MenuItem value="Date Descending">Date Descending</MenuItem>
+                                    <MenuItem value="Date Ascending">Date Ascending</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Box>
+                    </Box>
 
                     {sightingsData.map(sighting => (
                         <SightingCard
