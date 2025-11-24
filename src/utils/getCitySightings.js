78d059c3f10beccb397e5d8b592f1e0c7d36d157
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-11-24 04:03:06 +0000

    update getCitySightings to also get supabase data

--- src/utils/getCitySightings.js
+++ src/utils/getCitySightings.js
@@ -1,8 +1,16 @@
 import axios from 'axios';
+import { supabase } from '../supabase';
 
-export default function getCitySightings(city) {
-    return axios.get(`https://dev-task.elancoapps.com/data/tick-sightings/city/${city}`)
-        .then(function (response) {
-            return response.data
-        })
+export default async function getCitySightings(city) {
+    const apiResponse = await axios.get(`https://dev-task.elancoapps.com/data/tick-sightings/city/${city}`);
+    const apiSightings = apiResponse.data;
+
+    const { data: supabaseSightings, error } = await supabase
+        .from("tick_sightings")
+        .select("*")
+        .ilike("city", city);
+
+    if (error) throw error;
+
+    return [...apiSightings, ...(supabaseSightings || [])];
 }
