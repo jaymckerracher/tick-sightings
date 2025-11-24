9e69c386a5a0663b8e8a5eda2666475fbedfb408
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2025-11-24 03:59:52 +0000

    fix issues with supabase and vite
    - roll back to vite version that supports supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const URL = process.env.VITE_SUPABASE_URL;
-const KEY = process.env.VITE_SUPABASE_ANON_KEY;
+const URL = import.meta.env.VITE_SUPABASE_URL;
+const KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(URL, KEY);
--- src/utils/getAllSightings.js
+++ src/utils/getAllSightings.js
@@ -1,8 +1,15 @@
 import axios from "axios";
+import { supabase } from "../supabase";
 
-export default function getAllSightings() {
-    return axios.get('https://dev-task.elancoapps.com/data/tick-sightings')
-        .then(function (response) {
-            return response.data
-        })
+export default async function getAllSightings() {
+    const apiResponse = await axios.get('https://dev-task.elancoapps.com/data/tick-sightings')
+    const apiSightings = apiResponse.data;
+
+    const { data: supabaseSightings, error } = await supabase
+        .from("tick_sightings")
+        .select("*");
+
+    if (error) throw error;
+
+    return [...apiSightings, ...(supabaseSightings || [])];
 }
